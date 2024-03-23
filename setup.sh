e125ac753dce7618644f8c9ffad5e1d17ef7f732
ABC10946 <abctech10946@gmail.com>
2024-03-24 01:42:17 +0900

    change install path to configurable systemd path

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo cp -rf deepcool-ak620-digital.service /lib/systemd/system/
-sudo cp -rf deepcool-ak620-digital-restart.service /lib/systemd/system/
-sudo cp -rf deepcool-ak620-digital.py /usr/bin/deepcool-ak620-digital.py
+sudo cp -rf deepcool-ak620-digital.service /etc/systemd/system/
+sudo cp -rf deepcool-ak620-digital-restart.service /etc/systemd/system/
+sudo systemctl daemon-reload
 sudo systemctl enable deepcool-ak620-digital.service
 sudo systemctl enable deepcool-ak620-digital-restart.service
 sudo systemctl start deepcool-ak620-digital.service
